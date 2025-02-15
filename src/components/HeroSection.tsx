08274d0032113016abcee06d17762d4d923d0bd0
gomer <gaufogomer@gmail.com>
2025-02-15 11:44:26 +0800

    fixing UX

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@
 function HeroSection() {
 
     return (
-        <section className="relative flex justify-center items-center flex-col gap-10 h-full rounded-md bg-gradient-to-t from-[#56a5efd4] to-[#fafafa4d]  overflow-hidden">
+        <section className="relative h-[65svh] flex justify-center items-center flex-col gap-10 rounded-md bg-gradient-to-t from-[#56a5efd4] to-[#fafafa4d]  overflow-hidden md:h-[70svh]">
             <h1 className="text-3xl text-center md:text-5xl">Build <strong>Faster</strong> with <br />
                 Ready-to-Use <strong>Templates</strong>
             </h1>
