f5a654e4f068b3165d7ad1be692afeedf0d5e2cd
gomer <gaufogomer@gmail.com>
2025-02-18 00:08:08 +0800

    fixing UIs

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,28 +1,16 @@
 import React from 'react'
 
 interface FeatureCardProps {
+    logo: string;
     featureName: string;
     description: string;
 }
-const FeatureCard: React.FC<FeatureCardProps> = ({ featureName, description }) => {
+const FeatureCard: React.FC<FeatureCardProps> = ({ logo, featureName, description }) => {
 
     return (
         <div className="text-center p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 dark:bg-gray-900 ">
             <div className="flex justify-center">
-                <svg
-                    className="w-12 h-12 text-blue-600"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                </svg>
+               <img src={logo} alt="feature logo" className="w-12 h-12 " />
             </div>
             <h3 className="text-xl font-semibold mt-4 dark:text-[#FAFAFA]">{featureName}</h3>
             <p className="text-gray-600 mt-2 dark:text-[#FAFAFA]">
--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import FeatureCard from './FeatureCard'
+import Fast from '../assets/fast.svg';
+import EasyToUse from '../assets/easy.svg';
+import Customizable from '../assets/customizable.svg';
 
 function FeatureSection() {
     return (
@@ -8,14 +11,17 @@ function FeatureSection() {
                 <h2 className="text-3xl font-bold text-center mb-8 dark:text-[#FAFAFA]">Why Choose Us?</h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <FeatureCard
+                    logo={EasyToUse}
                         featureName='Easy to Use'
                         description='Our platform is designed to be intuitive and user-friendly, so you can get started right away.'
                     />
                     <FeatureCard
+                    logo={Fast}
                         featureName='Fast Performance'
                         description='Experience lightning-fast performance with our optimized infrastructure.'
                     />
                     <FeatureCard
+                    logo={Customizable}
                         featureName='Customizable'
                         description='Tailor the platform to your needs with customizable features and settings.'
                     />
