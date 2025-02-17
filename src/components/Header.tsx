a88e1fa026f9b20057938f3d35aebfe7fdca5b34
gomer <gaufogomer@gmail.com>
2025-02-17 22:55:34 +0800

    improving UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -52,11 +52,24 @@ function Header() {
                                 </div>
                             </div>
 
-                            <div className="mt-12 lg:mt-0">
-                                <a href="/register" className="relative bg-blue-900 flex border-1 rounded-full h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
-                                    <span className="relative text-sm font-semibold text-white"> Get Started</span>
-                                </a>
-                            </div>
+                          
+                            <button className="cursor-pointer text-sm font-bold transition-all duration-200 px-4 py-2 rounded-full bg-blue-900 border border-transparent flex items-center hover:bg-blue-950 active:scale-95 mt-0 group-data-[state=active]:mt-10">
+                                <span className="text-white">Get Started</span>
+                                <svg
+                                    width="27"
+                                    height="27"
+                                    viewBox="0 0 74 74"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-8 ml-2 transition-transform duration-300 ease-in-out group-hover:translate-x-1"
+                                >
+                                    <circle cx="37" cy="37" r="35.5" stroke="white" strokeWidth="3" />
+                                    <path
+                                        d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                                        fill="white"
+                                    />
+                                </svg>
+                            </button>
                         </div>
                     </div>
                 </div>
