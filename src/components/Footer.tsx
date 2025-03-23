d86c79379d8d41cdda35797ae84d75c7dc0ae997
gomer <gaufogomer@gmail.com>
2025-03-23 22:30:59 +0800

    style:(footer): improve UI?UX

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,9 +14,6 @@ function Footer() {
             <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
           </li>
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-          </li>
-          <li>
             <a href="#" className="hover:underline">Contact</a>
           </li>
         </ul>
